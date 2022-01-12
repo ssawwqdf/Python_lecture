ce206236e2254f3dc10d027ea2a37626c3b9ad2e
ssawwqdf <ssawwqdf@gmail.com>
2022-01-12 18:46:46 +0900

    220112

--- lec12_numpy.py
+++ lec12_numpy.py
@@ -29,6 +29,8 @@ import numpy as np # 100% np로 alias 준다.
 # print(df.info())
 
 ####################################################### 위로 주피터로 옮기기 완료
-df.todict
+# df.todict
+#
+# df.to_dict()
 
-df.to_dict()
+np.isnan
